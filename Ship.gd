d46aec8e4682fc7b567fc63abcf0af0e06ee9e28
Jorge Rodriguez <jlrm01@gmail.com>
2023-03-22 19:20:58 -0400

    Initial commit

---
+++ Ship.gd
@@ -0,0 +1,60 @@
+extends KinematicBody2D
+
+export (String) var ship_name = "Ship " + str(self.get_instance_id())
+
+signal ship_clicked_on
+
+onready var agent = $NavigationAgent2D
+onready var animation_player = $AnimationPlayer
+
+var velocity = Vector2.ZERO
+var speed = 800
+var target_position = null
+onready var sprite = $ShipA
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	yield(owner, "ready")
+
+	get_tree().call_group("Level", "connect_ship_signals", self)
+
+func _on_Ship_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+#			print("got a mouse event! " + ship_name)
+			emit_signal("ship_clicked_on", self)
+
+func _physics_process(delta):
+	if agent.is_navigation_finished():
+#		print("Navigation is done!")
+		target_position = null
+		return
+		
+	if not target_position:
+		return
+		
+	var next_location = agent.get_next_location()
+	var agent_position = global_transform.origin
+	
+	var direction = agent_position.direction_to(next_location)
+	
+	
+	velocity = speed * direction
+	agent.set_velocity(velocity)
+	
+func _on_Ship_mouse_entered():
+	animation_player.play("mouse_over")
+	get_tree().call_group("Level", "mouse_over", self)
+
+
+func _on_Ship_mouse_exited():
+	animation_player.play("mouse_exit")
+	get_tree().call_group("Level", "mouse_exit", self)
+
+func clicked():
+	animation_player.play("selected")
+	
+
+func _on_NavigationAgent2D_velocity_computed(safe_velocity):
+	sprite.rotation = safe_velocity.angle() + 90
+	velocity = move_and_slide(safe_velocity)
---
+++ level1.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+var current_ship: KinematicBody2D = null
+var mouse_is_over_ship = null
+
+func _ready():
+	pass
+
+func connect_ship_signals(instance):
+	instance.connect("ship_clicked_on", self, "on_ship_clicked")
+
+func _input(event):
+	if event is InputEventMouseButton:
+		print("clicked on screen!")
+		
+		setup_target_position(event.global_position)
+		
+func setup_target_position(position: Vector2):
+	if not current_ship:
+#		print("No ship selected!")
+		return
+
+	if not mouse_is_over_ship:		
+		current_ship.target_position = position
+		current_ship.agent.set_target_location(position)
+		
+func on_ship_clicked(instance):
+	current_ship = instance
+#	print("Current ship is now: " + instance.name)	
+	current_ship.clicked()
+	
+	print(current_ship)
+
+func mouse_over(instance):
+	mouse_is_over_ship = instance
+	
+func mouse_exit(instance):
+	mouse_is_over_ship = null
+	
