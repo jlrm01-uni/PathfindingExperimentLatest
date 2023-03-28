b1478ecfc003cf2942c7bb9220e4191a5be4441f
Jorge Rodriguez <jlrm01@gmail.com>
2023-03-27 22:30:23 -0400

    added follow leader

--- Ship.gd
+++ Ship.gd
@@ -10,16 +10,25 @@ onready var animation_player = $AnimationPlayer
 var velocity = Vector2.ZERO
 var speed = 200
 var target_position = null
+var previous_target_position = null
+
 onready var sprite = $ShipA
+
+onready var target = get_node(target_path)
+
 export (float) var slow_radius = 200
 
-export (NodePath) var target_path = null
+export (NodePath) var target_path = NodePath()
+export (float) var follow_offset = 200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	yield(owner, "ready")
 	agent.max_speed = speed
 	get_tree().call_group("Level", "connect_ship_signals", self)
+	
+	if target:
+		target_position = target.global_position
 
 func _on_Ship_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
@@ -28,6 +37,10 @@ func _on_Ship_input_event(viewport, event, shape_idx):
 			emit_signal("ship_clicked_on", self)
 
 func _physics_process(delta):
+	if target != self and target != null:
+		handle_follow_leader(delta)
+		return
+		
 	if agent.is_navigation_finished():
 #		print("Navigation is done!")
 		target_position = null
@@ -49,6 +62,30 @@ func _physics_process(delta):
 	
 	agent.set_velocity(new_velocity)
 	
+func handle_follow_leader(delta):
+	if not target_position:
+		return
+		
+	if agent.is_navigation_finished():
+#		set_physics_process(false)
+		return
+		
+#	if global_transform.origin.distance_to(target_position) < follow_offset:
+##		agent.set_velocity(Vector2.ZERO)
+#		return
+		
+	var next_location = agent.get_next_location()
+	var agent_position = global_transform.origin
+	
+	var direction = agent_position.direction_to(next_location)
+	
+#	var target_position_according_to_offset = agent_position.move_toward(target_position, follow_offset)
+	var new_velocity = Utils.arrive_to(velocity, agent_position, next_location, target_position, speed, slow_radius)
+#	sprite.rotation = new_velocity.angle() + 90
+#
+#	velocity = move_and_slide(new_velocity)
+	agent.set_velocity(new_velocity)
+	
 func _on_Ship_mouse_entered():
 	animation_player.play("mouse_over")
 	get_tree().call_group("Level", "mouse_over", self)
@@ -60,8 +97,31 @@ func _on_Ship_mouse_exited():
 
 func clicked():
 	animation_player.play("selected")
-	
+	agent.radius = 0
+	agent.neighbor_dist = 0
 
 func _on_NavigationAgent2D_velocity_computed(safe_velocity):
+#	if safe_velocity == Vector2.ZERO and target:
+#		pass
+		
 	sprite.rotation = safe_velocity.angle() + 90
 	velocity = move_and_slide(safe_velocity)
+
+
+func _on_Timer_timeout():
+	if target == self or target == null:
+		return
+	
+#	agent.avoidance_enabled = false
+	
+	target_position = target.global_position
+	
+#	if target_position == previous_target_position:
+#		return
+		
+	agent.target_desired_distance = follow_offset	
+	agent.set_target_location(target.global_position)
+	previous_target_position = target_position
+
+func _on_NavigationAgent2D_navigation_finished():
+	target_position = null
